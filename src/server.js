adad204924b817c0bd79017f613924a0d309ad87
Iann <iisackssonoliveira@gmail.com>
2020-08-04 20:09:49 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(3333, () => {
+  console.log('🚀️ Server started on port 3333!');
+});
