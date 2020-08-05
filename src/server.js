18dffac6bb0f1f4bb7c0930c2d4f3094a589501b
Iann <iisackssonoliveira@gmail.com>
2020-08-04 22:15:08 -0300

    chore: Add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import AppError from './errors/AppError';
 
@@ -7,8 +8,8 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.use((err, req, res, next) => {
